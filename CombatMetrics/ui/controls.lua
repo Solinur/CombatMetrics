de9e2c5eccc90fedc4cbbee50f6e9a23e3f9fdc6
Solinur <github@my-eg0.de>
2025-09-13 21:04:21 +0200

    add shared control pools

--- CombatMetrics/ui/controls.lua
+++ CombatMetrics/ui/controls.lua
@@ -4,15 +4,47 @@ local util = CMXint.util
 local ui = CMXint.ui
 local logger
 
-local ControlHandler = ZO_InitializingObject:Subclass()
-CMXint.ControlHandler = ControlHandler
+local function CreateSharedControlType(template)
+	local SharedControlType = ZO_Object:Subclass()
 
+	function SharedControlType:New(pool, objectKey)
+		local newLabel = ZO_ObjectPool_CreateControl(template, pool, CombatMetricsReport)
+		local controlMeta = getmetatable(newLabel)
+		newLabel = setmetatable(newLabel, {
+			__index = function(table, key)
+				local entry = controlMeta[key]
+				if entry ~= nil then
+					return entry
+				else
+					return self[key]
+				end
+			end
+		})
+		newLabel.__index = newLabel
+		newLabel:Initialize(pool, objectKey)
+		return newLabel
+	end
 
+	function SharedControlType:Initialize(pool, objectKey)
+		self.pool = pool
+		self.objectKey = objectKey
+	end
+
+	function SharedControlType:Release()
+		self:SetParent(CombatMetricsReport)
+		self.pool:ReleaseObject(self.objectKey)
+	end
+
+	return ZO_ObjectPool:New(SharedControlType, ZO_ObjectPool_DefaultResetControl)
+end
 
 local isFileInitialized = false
-function CMXint.InitializeEControlHandler()
+function CMXint.InitializeControlHandler()
 	if isFileInitialized == true then return false end
 	logger = util.initSublogger("Controls")
+
+	ui.sharedTextures = CreateSharedControlType("CombatMetrics_SharedTexture")
+	ui.sharedLabels = CreateSharedControlType("CombatMetrics_SharedLabel")
 
     isFileInitialized = true
 	return true
