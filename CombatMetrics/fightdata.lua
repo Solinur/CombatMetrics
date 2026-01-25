fd6dec9d8d3d63a0ec2022f0e30772886d049777
Solinur <github@my-eg0.de>
2026-01-25 23:36:25 +0100

    add function

--- CombatMetrics/fightdata.lua
+++ CombatMetrics/fightdata.lua
@@ -20,6 +20,18 @@ function util:GetEnemyUnits(units) -- TODO: Attach this to fight_data
 	return unitIds
 end
 
+function util:GetFriendlyUnits(units) -- TODO: Attach this to fight_data
+	local unitIds = {}
+
+	for unitId, unit in pairs(units) do
+		if unit.isFriendly then
+			unitIds[#unitIds + 1] = unitId
+		end
+	end
+
+	return unitIds
+end
+
 ---@class FightDataManager
 ---@field fights table<integer,Fight>
 ---@field data Fight?
